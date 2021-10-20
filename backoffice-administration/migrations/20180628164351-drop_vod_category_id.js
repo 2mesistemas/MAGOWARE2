8f4cf9378f9caa72320b8ac15324ed8a5c8cd516
2mesistemas <90299970+2mesistemas@users.noreply.github.com>
2021-10-20 00:24:41 -0300

    Delete backoffice-administration directory

--- backoffice-administration/migrations/20180628164351-drop_vod_category_id.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  up: function (queryInterface, Sequelize) {
-    queryInterface.removeColumn('vod', 'category_id').catch(function(err) {console.log('Dropping column vod.category_id failed with error message: ',err.message);});
-  }
-};
-
